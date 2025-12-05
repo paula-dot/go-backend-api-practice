0077219cef33be467d6a913d953960dc631b1856
Paul Ochieng <akelopauljudeochieng@gmail.com>
2025-12-05 22:36:36 +0300

    add MySQL database connection and initialization in main function

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,44 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/sikozonpc/ecom/cmd/api"
+	"github.com/sikozonpc/ecom/config"
+	"github.com/sikozonpc/ecom/db"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Net:                  "tcp",
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(":8080", db)
 	log.Println("Starting server on :8080")
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Connected to database")
 }
