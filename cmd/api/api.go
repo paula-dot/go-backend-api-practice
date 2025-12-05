44c00efe4001f921d87fb94a590b0b577dc44e58
Paul Ochieng <akelopauljudeochieng@gmail.com>
2025-12-05 20:57:21 +0300

    implement API server with user authentication routes

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,10 +1,30 @@
 package api
 
-import "database/sql"
+import (
+	"database/sql"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sikozonpc/ecom/service/user"
+)
 
 type APIServer struct {
 	addr string
 	db   *sql.DB
 }
 
-func NewAPIServer(addr string, db *sql.DB) *APIServer {}
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr, db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api").Subrouter()
+
+	userhandler := user.NewHandler()
+	userhandler.RegisterRoutes(subrouter)
+
+	return http.ListenAndServe(s.addr, router)
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,15 @@
-package cmd
+package main
+
+import (
+	"log"
+
+	"github.com/sikozonpc/ecom/cmd/api"
+)
+
+func main() {
+	server := api.NewAPIServer(":8080", nil)
+	log.Println("Starting server on :8080")
+	if err := server.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
